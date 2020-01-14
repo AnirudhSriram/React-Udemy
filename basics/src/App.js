46f87f7e421102c65828c75a96ee657ab4600c63
Anirudh Sriram <anirudh.sriram@siemensgamesa.com>
2020-01-14 15:29:54 +0530

    styled components

--- basics/src/App.js
+++ basics/src/App.js
@@ -5,39 +5,89 @@ class App extends React.Component {
   state = {
     Persons: [
       {
-        name: 'Anirudh',
+        id:0,
+        name: 'Terrorbladezz',
         age: 24
       },
       {
-        name: 'Prerana',
+        id:1,
+        name: 'Anti mage',
+        age: 23
+      },
+      {
+        id:2,
+        name: 'Templar assasin',
         age: 23
       }
-    ]
+    ],
+    showPersons: true
   }
 
-  switchNameHandler = (newName)=> {
-    let tempState = this.state.Persons;
-    tempState[1].name = newName;
-    this.setState(tempState)
+  deletePerson = (index) => {
+    console.log(index);
+    const persons = [...this.state.Persons]
+    persons.splice(index,1);
+    this.setState({Persons:persons});
   }
 
-  nameChangedHandler = (event) => {
-    let tempState = this.state.Persons;
-    tempState[1].name = event.target.value;
-    this.setState(tempState)
+  nameChangedHandler = (event,id) => {
+    const Persons = [...this.state.Persons];
+    let temp=  Persons[id];
+    temp.name = event.target.value;
+    Persons[id] = temp;
+    this.setState({Persons:Persons});
+    // this.setState({Persons : persons})
   }
+
+  togglePersons = () => {
+    this.setState({
+      showPersons: !this.state.showPersons
+    })
+  }
+  
   render() {
-    return (
-      <div className="App">
-        <h1>Hi I am a react app</h1>
-        <button onClick={this.switchNameHandler.bind(this,'Prerana KR')} style={{border:'2px solid purple',backgroundColor:'skyblue' }}>Switch Name</button>
-        <Person age={this.state.Persons[0].age} name={this.state.Persons[0].name} 
-          click={this.switchNameHandler.bind(this,'poo')}
-          change = {this.nameChangedHandler}
-        >My Sex is Male </Person>
-        <Person age={this.state.Persons[1].age} name={this.state.Persons[1].name}  change = {this.nameChangedHandler}/>
+    const buttonStyles={
+      border: '1px solid transparent',
+      color:'white',
+      padding:'15px',
+      backgroundColor:'green',
+      fontSize: '16px',
+      borderRadius:'5px',
+      ':hover' : {
+        backgroundColor:'lightgreen',
+        color:'black'
+      }
+    }
 
+    let persons = null;
+    if (this.state.showPersons) {
+      persons = <div>
+        {this.state.Persons.map((person,index) => <Person key={person.id} age={person.age} name={person.name} change={(event)=>this.nameChangedHandler(event,person.id)} click={()=>this.deletePerson(index)} /> )}
       </div>
+      buttonStyles.backgroundColor = 'red';
+      buttonStyles[':hover'] = {
+        backgroundColor:'salmon',
+        color:'black'
+      }
+    }
+
+    let classes = [];
+    if(this.state.Persons.length<=2){
+      classes.push('red');
+    }
+    if(this.state.Persons.length <=1){
+      classes.push('bold');
+    }
+    
+    
+    return (
+      <StyleRoot>
+      <div className="App">
+        <p className={classes.join(' ')}>This works</p>
+        {this.state.showPersons ? persons: <p>Click on toggle to view Persons</p>}
+        <button onClick={this.togglePersons} style={buttonStyles}>Toggle Persons</button>
+      </div>
+      </StyleRoot>
     );
   }
 
--- basics/src/Person/Person.js
+++ basics/src/Person/Person.js
@@ -1,11 +1,19 @@
 import React from 'react';
 import './Person.css'
+
 const Person = (props) => {
+    const style = {
+        '@media (min-width:500px)' : {
+            width : '450px',
+             backgroundColor:'pink'
+        }
+    }
     return (
-        <div className="Person">
-            <p onClick={props.click}>I'm  {props.name} and I am {props.age} years old</p>
+        <div className="Person" style={style}>
+            <p >I'm  {props.name} and I am {props.age} years old</p>
             <p>{props.children}</p>
             <input type='name' onChange={props.change} placeholder={props.name} />
+            <button onClick={props.click}>Delete</button>
         </div>
     )
 }
